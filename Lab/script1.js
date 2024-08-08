9bd79927fef956c04807521bb3dea0e6b9337831
QwertyVerttt <151605628+QwertyVerttt@users.noreply.github.com>
2024-08-08 15:35:34 +0800

    Add files via upload

---
+++ Lab/script1.js
@@ -0,0 +1,7 @@
+function buttonClick() {
+  alert("Biodata");
+  document.getElementById("test").innerHTML = "Nama : Agung";
+  document.getElementById("test1").innerHTML = "Nim : 105841102323 ";
+  document.getElementById("test2").innerHTML = "Kelas : Informatika 2-A ";
+  document.getElementById("test3").innerHTML = "Biodata";
+}
